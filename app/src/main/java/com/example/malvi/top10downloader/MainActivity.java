14e9915edd04e5d62884a113919bb3d5827e3141
malavika <malvi.malavika@gmail.com>
2018-07-19 12:06:08 -0400

    call async task

--- app/src/main/java/com/example/malvi/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/malvi/top10downloader/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.malvi.top10downloader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import academy.learnprogramming.top10downloader.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: starting Asynctask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL goes here");
+        Log.d(TAG, "onCreate: done");
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is " + s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: starts with " + strings[0]);
+            return "doInBackground completed.";
+        }
     }
 }
